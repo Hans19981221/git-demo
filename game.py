7315fecbb13ca6faaf89b64db6880fe71ad17379
hans <gg124678>
2024-01-03 21:23:48 +0800

    完成遊戲程式邏輯

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
 import random
 
-x = random.randint(1,50)
-
+x = random.randint(1, 50)
 print(x)
+
+for i in range(15):
+    y = int(input("猜數字0-50"))
+    if y == x:
+        print("正確")
+        break
+    else:
+        if y > x:
+            print("猜低一點")
+        else:
+            print("猜高一點")
+
+        print("錯")
