2e7f6c75236f535b93108051bf82b1fca6a6b94d
hans <gg124678>
2024-01-08 17:33:34 +0800

    修改txt

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ end = 50
 x = random.randint(start, end)
 print(f"範圍:{start}~{end}")
 
-#print(x)
+# print(x)
 
 
 for i in range(3):
@@ -17,16 +17,16 @@ for i in range(3):
     else:
         if y > x:
             print("猜低一點")
-            if end > y :
+            if end > y:
                 end = y - 1
         else:
-            if start  < y :
+            if start < y:
                 print("猜高一點")
             start = y + 1
 
         print("錯")
 
-if y!=x:
+if y != x:
     print(f"猜錯了。答案是:{x}")
 else:
     print(f"猜對了，共猜{i+1}次")
