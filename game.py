4afffb1e49784b973f859f44048b2607cc2feb9f
hans <gg124678>
2024-01-08 16:54:55 +0800

    完成區間提示功能

--- game.py
+++ game.py
@@ -1,17 +1,32 @@
 import random
 
-x = random.randint(1, 50)
-print(x)
+start = 1
+end = 50
 
-for i in range(15):
-    y = int(input("猜數字0-50"))
+x = random.randint(start, end)
+print(f"範圍:{start}~{end}")
+
+#print(x)
+
+
+for i in range(3):
+    y = int(input(f"猜數字0-50({start}~{end}):"))
     if y == x:
         print("正確")
         break
     else:
         if y > x:
             print("猜低一點")
+            if end > y :
+                end = y - 1
         else:
-            print("猜高一點")
+            if start  < y :
+                print("猜高一點")
+            start = y + 1
 
         print("錯")
+
+if y!=x:
+    print(f"猜錯了。答案是:{x}")
+else:
+    print(f"猜對了，共猜{i+1}次")
